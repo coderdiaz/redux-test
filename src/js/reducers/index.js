d8173b9c598b7dc79ed36d429548c6e63e7c984b
Yonatan Rojas S <yonatan@bedu.org>
2018-10-31 12:26:52 -0600

    applied actions in reducer

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,19 +1,30 @@
-import { ADD_ARTICLE, DELETE_ARTICLE } from "../constants/action-types"
+import {
+    ADD_ARTICLE,
+    DELETE_ARTICLE,
+    DONE_ARTICLE
+} from "../constants/action-types"
 
 const initialState = {
-    articles: []
+    articles: [],
+    completed: false
 };
+
 const rootReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_ARTICLE:
             return {
                 ...state,
-                articles: [...state.articles, action.payload]
+                articles: [...state.articles, action.payload],
             };
         case DELETE_ARTICLE:
             return {
                 ...state,
-                articles: state.articles.filter(article => article.id !== action.payload.id )
+                articles: state.articles.filter(article => article.id !== action.payload.id ),
+            }
+        case DONE_ARTICLE:
+            return {
+                ...state,
+                completed: !state.completed
             }
 
         default:
