f898169c1bba9f5cd350bb3997a273fd92a2e878
Yonatan Rojas S <yonatan@bedu.org>
2018-10-31 12:26:10 -0600

    created actions for the reducer

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,4 +1,8 @@
-import { ADD_ARTICLE, DELETE_ARTICLE } from '../constants/action-types';
+import { 
+    ADD_ARTICLE, 
+    DELETE_ARTICLE,
+    DONE_ARTICLE 
+} from '../constants/action-types';
 
 export const addArticle = (article) => ({
     type: ADD_ARTICLE,
@@ -7,5 +11,10 @@ export const addArticle = (article) => ({
 
 export const deleteArticle = (article) => ({
     type: DELETE_ARTICLE,
+    payload: article
+})
+
+export const doneArticle = (article) => ({
+    type: DONE_ARTICLE,
     payload: article
 })
